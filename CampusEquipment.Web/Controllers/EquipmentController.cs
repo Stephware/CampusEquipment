1ef217f2c4d7b99635781ec60c88097d2637a80e
Stephware <pinacate.stephen@gmail.com>
2026-09-12 14:49:08 +0800

    feat: add equipment search and filters

--- CampusEquipment.Web/Controllers/EquipmentController.cs
+++ CampusEquipment.Web/Controllers/EquipmentController.cs
@@ -1,4 +1,5 @@
 using CampusEquipment.Core.Services;
+using CampusEquipment.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CampusEquipment.Web.Controllers;
@@ -6,15 +7,40 @@ namespace CampusEquipment.Web.Controllers;
 public class EquipmentController : Controller
 {
     private readonly IEquipmentService _equipmentService;
+    private readonly IDepartmentService _departmentService;
 
-    public EquipmentController(IEquipmentService equipmentService)
+    public EquipmentController(
+        IEquipmentService equipmentService,
+        IDepartmentService departmentService)
     {
         _equipmentService = equipmentService;
+        _departmentService = departmentService;
     }
 
-    public async Task<IActionResult> Index()
+    public async Task<IActionResult> Index(
+        string? search,
+        string? category,
+        string? status,
+        int? departmentId)
     {
-        var equipment = await _equipmentService.GetAllEquipment();
-        return View(equipment);
+        var equipment = await _equipmentService.SearchEquipment(
+            search,
+            category,
+            status,
+            departmentId);
+
+        var departments = await _departmentService.GetAllDepartments();
+
+        var model = new EquipmentIndexViewModel
+        {
+            Equipment = equipment,
+            Departments = departments,
+            Search = search,
+            Category = category,
+            Status = status,
+            DepartmentId = departmentId
+        };
+
+        return View(model);
     }
 }
